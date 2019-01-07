4168f018cb891e6bdebca32888636b4e2fb8fbbf
Tarantini Pereira <taranttini@hotmail.com>
2019-01-07 08:12:08 -0200

    created firebase login

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
 // TODO: Replace with your project's customized code snippet
 var config = {
-  apiKey: "<API_KEY>",
-  authDomain: "<PROJECT_ID>.firebaseapp.com",
+  apiKey: "xx<API_KEY>",
+  authDomain: "xx<PROJECT_ID>.firebaseapp.com",
   databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-  projectId: "<PROJECT_ID>",
-  storageBucket: "<BUCKET>.appspot.com",
-  messagingSenderId: "<SENDER_ID>",
+  projectId: "xx<PROJECT_ID>",
+  storageBucket: "xx<BUCKET>.appspot.com",
+  messagingSenderId: "xx<SENDER_ID>",
 };
 firebase.initializeApp(config);
