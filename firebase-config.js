0f2fab2af7f3964688dbc5f84164e6a833e68e08
Tarantini Pereira <taranttini@hotmail.com>
2019-01-10 01:44:36 -0200

    update

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@ var config = {
   apiKey: "<API_KEY>",
   authDomain: "<PROJECT_ID>.firebaseapp.com",
   databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-  projectId: "<PROJECT_ID>",
+  projectId: "UN<PROJECT_ID>",
   storageBucket: "<BUCKET>.appspot.com",
   messagingSenderId: "<SENDER_ID>",
 };
