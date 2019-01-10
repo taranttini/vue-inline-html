494e70bfc7fbacbe5a1dd11c59df5a47676ed3f5
Tarantini Pereira <taranttini@hotmail.com>
2019-01-10 01:30:35 -0200

    update

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 // Initialize Firebase
 // TODO: Replace with your project's customized code snippet
 var config = {
-  apiKey: "<API_KEY>",
+  apiKey: "XX<API_KEY>",
   authDomain: "<PROJECT_ID>.firebaseapp.com",
   databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
   projectId: "<PROJECT_ID>",
