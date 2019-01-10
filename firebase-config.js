76f1b209cde6802b0ffb88cb17c9172a22708672
Tarantini Pereira <taranttini@hotmail.com>
2019-01-10 01:41:28 -0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+  apiKey: "ZZ<API_KEY>",
+  authDomain: "SS<PROJECT_ID>.firebaseapp.com",
+  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  projectId: "LL<PROJECT_ID>",
+  storageBucket: "<BUCKET>.appspot.com",
+  messagingSenderId: "<SENDER_ID>",
+};
+firebase.initializeApp(config);
